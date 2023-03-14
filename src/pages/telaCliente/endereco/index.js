d1c00e0314b3fabd9f35d166f8ed7898053eadc9
VitorRekobe <vitorgabrielsalesgarcia7@gmail.com>
2023-03-14 12:39:49 -0300

    telas endereco feita

--- src/pages/telaCliente/endereco/index.js
+++ src/pages/telaCliente/endereco/index.js
@@ -10,7 +10,17 @@ function pageClienteEndereco() {
             <div className="principalCliente">
                 <OpcoesCliente></OpcoesCliente>
                 <div className="telaO">
- 
+                <div className="informaçõesClienteEndereco">
+                            {/* <input id="CEP" type="text" className="StyleInputUser" placeholder="CEP" /> */}
+                            <input id="Estado" type="password" className="StyleInputUser" placeholder="Estado" />
+                            <input id="Cidade" type="password" className="StyleInputUser" placeholder="Cidade" />
+                            <input id="Rua" type="email" className="StyleInputUser" placeholder="Rua" />
+                            <label for="numberHouse">Numero da Casa/apto:
+                                <input id="numberHouse" type="number" min='0' className="StyleInputUser" />
+                            </label>
+
+                            <button className="Botao" onClick={"Salvar"}>Salvar</button>
+                        </div>
                 </div>
             </div>
         </>
--- src/pages/telaCliente/favoritos/index.js
+++ src/pages/telaCliente/favoritos/index.js
@@ -1,17 +1,20 @@
 import OpcoesCliente from "../../../componentes/divClienteOp/divClienteOp";
 import './pageClienteFavoritos.css';
 import Header from "../../../componentes/Header/Header";
-import { FcLike } from "react-icons/fc";
+import { IoHeartOutline } from "react-icons/io5";
 
 function pageClienteFavoritos() {
-    
+
     return (
         <>
             <Header></Header>
             <div className="principalCliente">
                 <OpcoesCliente></OpcoesCliente>
                 <div className="telaO">
-                    <FcLike></FcLike>
+                    <div id="divFavoritos">
+                        
+                        <h2>Favoritos  Vazio <IoHeartOutline color="#E8D1C5" />  </h2>
+                    </div>
                 </div>
             </div>
         </>
