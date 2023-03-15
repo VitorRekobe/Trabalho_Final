886d2ecd52ca674ca4d23f621a61146562c29e50
Rafael Guilherme <rafael.guilherme.s.s@hotmail.com>
2023-03-15 23:44:48 +0000

    Update index.js and pageClientePedidos.css

--- src/pages/telaCliente/pedido/index.js
+++ src/pages/telaCliente/pedido/index.js
@@ -1,20 +1,21 @@
 import OpcoesCliente from "../../../componentes/divClienteOp/divClienteOp";
-import './pageClientePedidos.css';
+import "./pageClientePedidos.css";
 import Header from "../../../componentes/Header/Header";
 
 function pageClientePedido() {
-    
-    return (
-        <div className="background">
-            <Header></Header>
-            <div className="principalCliente">
-                <OpcoesCliente></OpcoesCliente>
-                <div className="telaO">
- 
-                </div>
-            </div>
+  return (
+    <div className="background">
+      <Header></Header>
+      <div className="principalCliente">
+        <OpcoesCliente></OpcoesCliente>
+        <div className="telaO">
+          <div id="divPedidos">
+            <h2>Não há pedidos</h2>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default pageClientePedido;
